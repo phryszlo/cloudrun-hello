a32ece3db3a2be1200873ec41268b7fcf10da84a
Daren Porter <daren.porter@gmail.com>
2022-11-08 16:16:23 -0500

    initial, with smurf

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+app.get('/smurf', (req, res) => {
+  res.send(`Welcome to the secret Smurf route`);
+})
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
